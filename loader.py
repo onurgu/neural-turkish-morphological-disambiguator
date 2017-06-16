6b9d82529eedf08d0d1328d3791e35e6a8686bcd
onur gungor <onurgu@gmail.com>
2017-06-16 06:59:58 +0300

    Loader bug fix

--- loader.py
+++ loader.py
@@ -5,12 +5,11 @@ from collections import defaultdict as dd
 
 from keras.utils.np_utils import to_categorical
 
+import sys
 
-def read_datafile(path):
-    word = []
-    sentences = []
-    # label2ids = {'surface_form': dd(int),
-    #              'morph_token': dd(int)}
+
+def read_datafile(train_path, test_path=None, sentence_length_limit=66):
+
     label_classes = ['surface_form',
                      'root',
                      'morph_token',
@@ -53,66 +52,82 @@ def read_datafile(path):
         """
         _encode_label(dictionary_name, label, label2ids)
 
-    with codecs.open(path, mode="r") as f:
-        line = f.readline()
-        while line:
-            tokens = line.split(" ")
-            surface_form = tokens[0].decode('utf-8')
-            encode_label('surface_form', surface_form)
-            analyses = [t.decode('utf-8') for t in tokens[1:]]
-            if surface_form in ["<DOC>", "<TITLE>", "</DOC>", "</TITLE>"]:
-                pass # do nothing, skip line
-            elif surface_form == "<S>":
-                sentence = [] # prepare the sentence variable
-            elif surface_form == "</S>":
-                # record the sentence
-                assert len(sentence) > 0, "sentence with length 0?"
-                roots = []
-                morph_tokens = []
-                for w in sentence:
-                    w_roots = []
-                    w_affixes = []
-                    for analysis in w[1:]:
-                        w_roots.append(analysis[0])
-                        w_affixes.append(analysis[1:])
-                    roots.append(w_roots)
-                    morph_tokens.append(w_affixes)
-                processed_sentence = {'sentence_length': len(sentence),
-                             'surface_forms': [w[0] for w in sentence],
-                             'surface_form_lengths': [len(w[0]) for w in sentence],
-                             'roots': roots,
-                             'root_lengths': [[len(root) for root in word] for word in roots],
-                             'morph_tokens': morph_tokens,
-                             'morph_token_lengths': [[len(morph_tokens) for morph_tokens in word] for word in morph_tokens]}
-                if len(sentence) > max_sentence_length:
-                    max_sentence_length = len(sentence)
-                if max([len(w[0]) for w in sentence]) > max_surface_form_length:
-                    max_surface_form_length = max([len(w[0]) for w in sentence])
-                if max([max(root_lengths) for root_lengths in processed_sentence['root_lengths']]) > max_word_root_length:
-                    max_word_root_length = max([max(root_lengths) for root_lengths in processed_sentence['root_lengths']])
-                if max([len(w[1:]) for w in sentence]) > max_n_analysis:
-                    max_n_analysis = max([len(w[1:]) for w in sentence])
-                if max([len(analysis[1:]) for w in sentence for analysis in w[1:]]) > max_analysis_length:
-                    max_analysis_length = max([len(analysis[1:]) for w in sentence for analysis in w[1:]])
-                # print processed_sentence
-                sentences.append(processed_sentence)
-            else:
-                # this is a legit surface form, extract morph. analyses
-                encode_label('surface_form', surface_form)
-                characters_seen = characters_seen.union(set(surface_form))
-                word.append(surface_form)
-                for morph_analysis in analyses:
-                    morph_tokens = morph_analysis.split("+")
-                    encode_label('root', morph_tokens[0])
-                    for morph_token in morph_tokens[1:]:
-                        encode_label('morph_token', morph_token)
-                    root = morph_tokens[0]
-                    morph_tokens = morph_tokens[1:]
-                    word.append([root] + morph_tokens)
-                # print word
-                sentence.append(word)
+    train_and_test_sentences = []
+    for path in [train_path, test_path]:
+        if path:
+            with codecs.open(path, mode="r") as f:
                 word = []
-            line = f.readline()
+                sentences = []
+                line = f.readline()
+                while line:
+                    tokens = line.split(" ")
+                    surface_form = tokens[0].decode('utf-8')
+                    encode_label('surface_form', surface_form)
+                    analyses = [t.decode('utf-8') for t in tokens[1:]]
+                    if surface_form in ["<DOC>", "<TITLE>", "</DOC>", "</TITLE>"]:
+                        pass # do nothing, skip line
+                    elif surface_form == "<S>":
+                        sentence = [] # prepare the sentence variable
+                    elif surface_form == "</S>":
+                        # record the sentence
+                        if len(sentence) == 0:
+                            print "sentence with length 0? %s %s" % (path, str(sentence))
+                            line = f.readline()
+                            continue
+                        if len(sentence) > sentence_length_limit:
+                            sys.stdout.write(".")
+                            line = f.readline()
+                            continue
+                        roots = []
+                        morph_tokens = []
+                        for w in sentence:
+                            w_roots = []
+                            w_affixes = []
+                            for analysis in w[1:]:
+                                # assert len(analysis) > 0, "analysis: %s %s" % (str(analysis), str(sentence))
+                                w_roots.append(analysis[0])
+                                w_affixes.append(analysis[1:])
+                            roots.append(w_roots)
+                            morph_tokens.append(w_affixes)
+                        processed_sentence = {'sentence_length': len(sentence),
+                                     'surface_forms': [w[0] for w in sentence],
+                                     'surface_form_lengths': [len(w[0]) for w in sentence],
+                                     'roots': roots,
+                                     'root_lengths': [[len(root) for root in word_root] for word_root in roots],
+                                     'morph_tokens': morph_tokens,
+                                     'morph_token_lengths': [[len(morph_tokens) for morph_tokens in word_morph_token] for word_morph_token in morph_tokens]}
+                        if len(sentence) > max_sentence_length:
+                            max_sentence_length = len(sentence)
+                        if max([len(w[0]) for w in sentence]) > max_surface_form_length:
+                            max_surface_form_length = max([len(w[0]) for w in sentence])
+                        if max([max(root_lengths) for root_lengths in processed_sentence['root_lengths']]) > max_word_root_length:
+                            max_word_root_length = max([max(root_lengths) for root_lengths in processed_sentence['root_lengths']])
+                        if max([len(w[1:]) for w in sentence]) > max_n_analysis:
+                            max_n_analysis = max([len(w[1:]) for w in sentence])
+                        if max([len(analysis[1:]) for w in sentence for analysis in w[1:]]) > max_analysis_length:
+                            max_analysis_length = max([len(analysis[1:]) for w in sentence for analysis in w[1:]])
+                        # print processed_sentence
+                        sentence = []
+                        sentences.append(processed_sentence)
+                    else:
+                        # this is a legit surface form, extract morph. analyses
+                        encode_label('surface_form', surface_form)
+                        characters_seen = characters_seen.union(set(surface_form))
+                        word.append(surface_form)
+                        for morph_analysis in analyses:
+                            morph_tokens = morph_analysis.split("+")
+                            encode_label('root', morph_tokens[0])
+                            for morph_token in morph_tokens[1:]:
+                                encode_label('morph_token', morph_token)
+                            root = morph_tokens[0]
+                            morph_tokens = morph_tokens[1:]
+                            word.append([root] + morph_tokens)
+                        # print word
+                        sentence.append(word)
+                        word = []
+                    line = f.readline()
+                train_and_test_sentences.append(sentences)
+                print "file processed"
 
     for c in characters_seen:
         encode_label("character", c)
@@ -123,7 +138,7 @@ def read_datafile(path):
               ["max_n_analysis", max_n_analysis],
               ["max_analysis_length", max_analysis_length]]:
         label2ids[m_str] = m_value
-    return sentences, label2ids
+    return train_and_test_sentences, label2ids
 
 import numpy as np
 
@@ -164,6 +179,19 @@ def encode_sentence(sentence, label2ids):
 
     # TODO: randomize the order of the analyses.
 
+    random_positions = np.random.randint(0, label2ids["max_n_analysis"], sentence_length)
+    for i in range(sentence_length):
+        pos = random_positions[i]
+        temp = np.copy(sentences_word_root_input[i, 0, :])
+        sentences_word_root_input[i, 0, :] = sentences_word_root_input[i, pos, :]
+        sentences_word_root_input[i, pos, :] = temp
+
+        temp = np.copy(sentences_analysis_input[i, pos, :])
+        sentences_analysis_input[i, pos, :] = sentences_analysis_input[i, 0, :]
+        sentences_analysis_input[i, 0, :] = temp
+
+        correct_tags_input[i] = pos
+
     # print sentences_word_root_input
     return sentences_word_root_input, sentences_analysis_input, surface_form_input, correct_tags_input
 
@@ -171,6 +199,11 @@ def encode_sentence(sentence, label2ids):
 
 if __name__ == "__main__":
     sentences, label2ids = read_datafile("sample.data")
-    encode_sentence(sentences[0], label2ids)
-    print sentences
+    encode_sentence(sentences[0][0], label2ids)
+    print sentences[0]
+    print label2ids
+
+    sentences, label2ids = read_datafile("train.merge.utf8", "test.merge.utf8")
+    encode_sentence(sentences[0][0], label2ids)
+    print sentences[0]
     print label2ids
